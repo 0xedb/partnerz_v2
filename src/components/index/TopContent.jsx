6820dc3472a13ac64108a9b8f3965af1f7305ffb
theBashShell <edohbruno@gmail.com>
2019-12-27 16:56:33 +0000

    dimensions fixed

--- src/components/index/TopContent.jsx
+++ src/components/index/TopContent.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {
   content,
-  container, info, 
-  filter
+  container,
+  info,
+  filter,
 } from './topcontent.module.css'
 import { useLozad } from '../../hooks/lozad'
 import background_mob from '../../assets/images/img/main_bg_mob.jpg'
@@ -12,22 +13,23 @@ const TopContent = () => {
   useLozad()
   return (
     <div className={container}>
-      <div>
-        <picture
-          className="lozad"
-          data-alt="background image"
-        >
-          <source
-            srcset={background_desk}
-            media="(min-width: 980px)"
-          />
-          <source
-            srcset={background_mob}
-            media="(min-width: 0px)"
-          />
-        </picture>
-      </div>
-      <div className={info}>another one</div>
+      <picture
+        className="lozad"
+        data-alt="background image"
+      >
+        <source
+          srcset={background_desk}
+          media="(min-width: 750px)"
+        />
+        <source
+          srcset={background_mob}
+          media="(min-width: 0px)"
+        />
+      </picture>
+
+      <section className={info}>
+        <div>mizess</div>
+      </section>
     </div>
   )
 }
