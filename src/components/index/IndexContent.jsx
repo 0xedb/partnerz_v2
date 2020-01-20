65e61a7bdbde6b2e4a41fe8b23a402ac7571cab0
theBashShell <edohbruno@gmail.com>
2020-01-20 14:00:41 +0000

    ...

--- src/components/index/IndexContent.jsx
+++ src/components/index/IndexContent.jsx
@@ -6,26 +6,32 @@ import {
 
 const IndexContent = () => {
   return (
-    <main className={main}>
-      <div>
-        Financial stability for everyone, by the
-        experts
-      </div>
-      <div className={firm}>
+    <>
+      <main className={main}>
         <div>
-          <img
-            src="https://api.iconify.design/vaadin:quote-left.svg"
-            alt="quote"
-          />
+          Financial stability for everyone, by the
+          experts
         </div>
-        Partnerz FP is an advisory firm with a
-        daring quest to use financial structures
-        to affect lives positively. We are
-        responsible for crafting world-class
-        financial solutions that touches
-        individuals, corporates, and governments.
-      </div>
-    </main>
+        <div className={firm}>
+          <div>
+            <img
+              src="https://api.iconify.design/vaadin:quote-left.svg"
+              alt="quote"
+            />
+          </div>
+          Partnerz FP is an advisory firm with a
+          daring quest to use financial structures
+          to affect lives positively. We are
+          responsible for crafting world-class
+          financial solutions that touches
+          individuals, corporates, and
+          governments.
+        </div>
+      </main>
+      <section>
+      
+      </section>
+    </>
   )
 }
 
