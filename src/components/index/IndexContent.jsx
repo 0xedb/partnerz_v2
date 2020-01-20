ce7ae0b9343c40c2fb48b4a0b4659870351dcedb
theBashShell <edohbruno@gmail.com>
2020-01-20 13:56:34 +0000

    quote added

--- src/components/index/IndexContent.jsx
+++ src/components/index/IndexContent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { main } from './indexcontent.module.css'
+import {
+  main,
+  firm,
+} from './indexcontent.module.css'
 
 const IndexContent = () => {
   return (
@@ -7,6 +10,20 @@ const IndexContent = () => {
       <div>
         Financial stability for everyone, by the
         experts
+      </div>
+      <div className={firm}>
+        <div>
+          <img
+            src="https://api.iconify.design/vaadin:quote-left.svg"
+            alt="quote"
+          />
+        </div>
+        Partnerz FP is an advisory firm with a
+        daring quest to use financial structures
+        to affect lives positively. We are
+        responsible for crafting world-class
+        financial solutions that touches
+        individuals, corporates, and governments.
       </div>
     </main>
   )
